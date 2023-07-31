07e7928b34666b43d3e294d4932e0156e1d72b45
Braden Wittkop <bradenjwittkop@gmail.com>
2023-07-31 11:17:41 -0500

    added java exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,40 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Java is \"fun\"");
+        String str = "hello";
+//        System.out.printf("I will tell you: %s", str);
+        int myFavoriteNumber = 12;
+        System.out.println(myFavoriteNumber);
+        String myString = "Hello";
+        System.out.println(myString);
+        myString = "goodbye";
+        System.out.println(myString);
+        long mySecondNumber = 123;
+        System.out.println(mySecondNumber);
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+//        int y = 5;
+//        System.out.println(y++);
+//        System.out.println(y);
+//        int x = 6;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        int x = 4;
+//        System.out.println(x++);
+//        System.out.println(x);
+        int x = 3;
+        int y = 4;
+        y = y * x;
+        System.out.println(y);
+        int a = 10;
+        int b = 2;
+        a = a / b;
+        b = b - a;
+        System.out.println(b);
+
+
+
+
     }
 }
