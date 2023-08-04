10a35c7a6e607d497702cb841a9ab01561ae3513
Braden Wittkop <bradenjwittkop@gmail.com>
2023-08-04 15:13:28 -0500

    added person exercise:

---
+++ src/util/Input.java
@@ -0,0 +1,28 @@
+package util;
+
+import java.util.Scanner;
+
+
+//Create a package inside of src named util. Inside of util, create a class named Input that has a private field named scanner. When an instance of this object is created, the scanner field should be set to a new instance of the Scanner class. The class should have the following methods, all of which return command line input from the user:
+//
+//		String getString()
+//		boolean yesNo()
+//		int getInt(int min, int max)
+//		int getInt()
+//		double getDouble(double min, double max)
+//		double getDouble()
+//		The yesNo method should return true if the user enters y, yes, or variants thereof, and false otherwise.
+//
+//		The getInt(int min, int max) method should keep prompting the user for input until they give an integer within the min and max. The getDouble method should do the same thing, but with decimal numbers.
+//
+//		Create another class named InputTest that has a static main method that uses all the methods from the Input class.
+
+
+public class Input {
+	private Scanner sc = new Scanner(System.in);
+
+
+	public static boolean yesNo() {
+		if (userInput == charAt(0));
+	}
+}
